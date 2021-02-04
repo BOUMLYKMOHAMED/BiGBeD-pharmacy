82e65dfe135970004761fa13fc535d6d1508aa04
boumlyk mohamed <boumlykhassan@gmail.com>
2021-02-04 12:29:10 +0100

    add activities

---
+++ app/src/main/java/master/STRI/bigbedpharmacie/EspaceActivity.java
@@ -0,0 +1,14 @@
+package master.STRI.bigbedpharmacie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EspaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_espace);
+    }
+}
